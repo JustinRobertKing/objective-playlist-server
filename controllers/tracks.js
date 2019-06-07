c522559bfce12cc7583c53ceb449991931b58f46
JustinRobertKing <kingj4@spu.edu>
2019-06-07 16:39:51 -0700

    set up a successful server with oauth

---
+++ controllers/tracks.js
@@ -0,0 +1,8 @@
+require('dotenv').config()
+const express = require('express');
+const router = express.Router();
+const db = require('../models')
+
+
+
+module.exports = router
---
+++ index.js
@@ -0,0 +1,11 @@
+require('dotenv').config()
+let express = require('express')
+let request = require('request')
+let app = express()
+
+
+
+
+
+
+app.listen(process.env.PORT || 3000)
