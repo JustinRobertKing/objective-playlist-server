3edcfe6f93cda2c0b2156074e88a01d20b465840
JustinRobertKing <kingj4@spu.edu>
2019-06-08 16:28:54 -0700

    database seeded

--- controllers/tracks.js
+++ controllers/tracks.js
@@ -6,11 +6,16 @@ const db = require('../models')
 router.post('/', (req, res) => {
 	console.log('in POST route... THIS IS FUCKING STUPID')
 	let input = req.body.map((t, i) => {
-		return {artist: t[0], title: t[1]}
+		return {artist: t[0], title: t[1], analytics: t[2]}
 	})
 	console.log('INPUUUUUUUT', input)
-	// db.Track.insertMany()	
-	res.send(req.body)
+	db.Track.insertMany(input)
+	.then(newDocs => {
+		res.send(newDocs)
+	})
+	.catch((error) => {
+		console.log('error', error)
+	})
 })
 
 module.exports = router
